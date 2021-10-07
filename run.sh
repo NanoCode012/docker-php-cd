961a6a856f5d9e4253ec07ec6bd5e897781a1136
NanoCode012 <kevinvong@rocketmail.com>
2021-10-07 19:24:27 +0700

    Remove space

--- run.sh
+++ run.sh
@@ -24,7 +24,7 @@ docker-compose down
 
 # Choose the first for simple deploy
 #        the second with SSL (used with nginx-proxy)
-docker-compose up -d 
+docker-compose up -d
 # docker-compose up -d -f docker-compose.yml -f docker-compose.ssl.yml
 
 # -------------------------------------------------------------------------- #
