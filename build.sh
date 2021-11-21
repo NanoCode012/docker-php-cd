0e343cdeed85d9a648f8e951f172a835d845f670
NanoCode012 <kevinvong@rocketmail.com>
2021-11-22 02:02:44 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,30 +3,30 @@
 # Stop if error
 set -e
 
-# Only run if container(s) is UP
-if docker-compose ps | grep -q "Up" ; then
-    echo 'Container is currently running. Building container(s)..'
-    #docker-compose build
-    docker-compose down
-fi
-
 # Choose the first for simple deploy
 #        the second to use Composer
 #        the third to use SSL with nginx-proxy
 #        the fourth to use all
 
-cmd='docker-compose -f docker-compose.yml '
+cmd='docker-compose '
+CONF='-f docker-compose.yml '
 
 if [ "$USE_COMPOSER" = true ] ; then
     echo 'Using Composer'
-    cmd+='-f docker-compose.composer.yml '
+    CONF+='-f docker-compose.composer.yml '
 fi
 
 if [ "$USE_SSL" = true ] ; then
     echo 'Using SSL'
-    cmd+='-f docker-compose.ssl.yml '
+    CONF+='-f docker-compose.ssl.yml '
 fi
 
-cmd+='up --build -d'
+# Only run if container(s) is UP
+if docker-compose ps | grep -q "Up" ; then
+    echo 'Container is currently running. Building container(s)..'
+    docker-compose build ${CONF}
+    docker-compose down
+fi
 
+cmd+="${CONF} up -d"
 echo $(eval "$cmd")
