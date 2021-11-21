d995c5d43ba2cfc929e599c7360e9ea7b672b153
NanoCode012 <kevinvong@rocketmail.com>
2021-11-22 02:30:18 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ fi
 # Only run if container(s) is UP
 if docker-compose ps | grep -q "Up" ; then
     echo 'Container is currently running. Building container(s)..'
-    docker-compose ${CONF} build 
+    docker-compose ${CONF} build --no-cache
     docker-compose down
 fi
 
